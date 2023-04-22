8a4136788748b7a79d39260c552ea25f6210a9a5
unknown <tobiiduarte95@gmail.com>
2023-04-22 19:01:19 -0300

    more practice

--- main.js
+++ main.js
@@ -1,29 +1,68 @@
 const tituloModificado = (titulo) => {
-    const title = document.getElementById("title")
+	const title = document.getElementById("title");
 
-    title.innerHTML = `${titulo}`
-}
+	title.innerHTML = `${titulo}`;
+};
 
-tituloModificado("ahora vamos a modificar todo el html mediante javascript")
+tituloModificado("ahora vamos a modificar todo el html mediante javascript");
 
 const pModificado = (parrafox) => {
-    const parrafo = document.getElementsByClassName("parrafo-modificar")
-    parrafo.innerHTML = `${parrafox}`
+	const parrafo = document.getElementsByClassName("parrafo-modificar");
+	parrafo.innerHTML = `${parrafox}`;
 
-    console.log(parrafo);
-}
+	console.log(parrafo);
+};
 
-pModificado("que pasa? se modifica el parrafo o no????")
+const pModificadoNameTwo = () => {
+	const parrafox = (lolaxco) => {
+		return lolaxco.name
+			? console.log(`hola soy tobi`)
+			: console.log(`Chau no soy tobi`);
+	};
+};
 
+const newFunction = () => {
+	console.log(`funcion en una linea`);
+};
 
+const otherNewFunction = () => {
+	return lolalolaoloala;
+};
+
+const otherNewNewFunction = () => {
+	return (newfuncion = () => {
+		console.log(`Funca esto?`);
+	});
+};
+
+const logfunction = () => {
+	console.log(`Hola osy tobiii`);
+};
+
+console.log(otherNewFunction);
+pModificado("que pasa? se modifica el parrafo o no????");
 
 const mostrarAlert = () => {
-    document.getElementsByClassName("btn-enviar");
-    return swal(`Oops! no hay nada que enviar`)
-}
+	document.getElementsByClassName("btn-enviar");
+	return swal(`Oops! no hay nada que enviar`);
+};
+const novedadesForm = document.getElementById("label-novedades");
+
+novedadesForm.onclick = "alertCart";
 
 const alertCart = () => {
-    document.getElementById("btn-cart")
-    return swal('Opps!', 'Su carrito está vacío :(', 'error')
-}
+	// document.getElementById("btn-cart")
+	return swal("Opps!", "Su carrito está vacío :(", "error");
+};
 
+console.dir(novedadesForm);
+
+const formulario = document.getElementById("sec-two-form");
+
+const labelNovedades = document.getElementById("novedades");
+
+console.dir(labelNovedades);
+
+const navHeader = document.getElementsByClassName("nav-header");
+
+console.dir(navHeader);
