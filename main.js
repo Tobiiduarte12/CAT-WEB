ab208ced90bdbe694f76ee743d0b67dda215b4c8
unknown <tobiiduarte95@gmail.com>
2023-04-24 16:54:27 -0300

    contador

--- main.js
+++ main.js
@@ -1,68 +1,22 @@
-const tituloModificado = (titulo) => {
-	const title = document.getElementById("title");
+function contadorExtra() {
+	const btnAumentar = document.querySelector(".btn-aumentar");
+	const btnDisminuir = document.querySelector(".btn-disminuir");
+	const resultado = document.getElementById("resultado");
 
-	title.innerHTML = `${titulo}`;
-};
+	let contador = 0;
 
-tituloModificado("ahora vamos a modificar todo el html mediante javascript");
-
-const pModificado = (parrafox) => {
-	const parrafo = document.getElementsByClassName("parrafo-modificar");
-	parrafo.innerHTML = `${parrafox}`;
-
-	console.log(parrafo);
-};
-
-const pModificadoNameTwo = () => {
-	const parrafox = (lolaxco) => {
-		return lolaxco.name
-			? console.log(`hola soy tobi`)
-			: console.log(`Chau no soy tobi`);
+	const aumentar = () => {
+		contador++;
+		resultado.textContent = contador;
 	};
-};
 
-const newFunction = () => {
-	console.log(`funcion en una linea`);
-};
+	const disminuir = () => {
+		contador--;
+		resultado.textContent = contador;
+	};
 
-const otherNewFunction = () => {
-	return lolalolaoloala;
-};
+	btnAumentar.addEventListener("click", aumentar);
+	btnDisminuir.addEventListener("click", disminuir);
+}
 
-const otherNewNewFunction = () => {
-	return (newfuncion = () => {
-		console.log(`Funca esto?`);
-	});
-};
-
-const logfunction = () => {
-	console.log(`Hola osy tobiii`);
-};
-
-console.log(otherNewFunction);
-pModificado("que pasa? se modifica el parrafo o no????");
-
-const mostrarAlert = () => {
-	document.getElementsByClassName("btn-enviar");
-	return swal(`Oops! no hay nada que enviar`);
-};
-const novedadesForm = document.getElementById("label-novedades");
-
-novedadesForm.onclick = "alertCart";
-
-const alertCart = () => {
-	// document.getElementById("btn-cart")
-	return swal("Opps!", "Su carrito está vacío :(", "error");
-};
-
-console.dir(novedadesForm);
-
-const formulario = document.getElementById("sec-two-form");
-
-const labelNovedades = document.getElementById("novedades");
-
-console.dir(labelNovedades);
-
-const navHeader = document.getElementsByClassName("nav-header");
-
-console.dir(navHeader);
+contadorExtra();
